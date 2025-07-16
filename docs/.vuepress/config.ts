f99452723e9a583c31749bd7a5fa64d1523d7a2a
marcgrue <marcgrue@gmail.com>
2025-07-16 15:19:13 +0200

    Commiting relevant files

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,18 +4,13 @@ import theme from "./theme.js";
 
 
 export default defineUserConfig({
-  base: "/",
-  lang: "en-US",
-  title: "Molecule",
-  description: "A docs demo for vuepress-theme-hope",
-  theme,
-  // head: [
-  //   [
-  //     "link",
-  //     {
-  //       rel: "stylesheet",
-  //       href: "/css/custom.css",
-  //     },
-  //   ],
-  // ],
+    base: "/",
+    lang: "en-US",
+    title: "Molecule",
+    description: "A docs demo for vuepress-theme-hope",
+    theme,
+
+    head: [
+        ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ]
 });
